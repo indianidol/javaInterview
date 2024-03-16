ef51a7553569efd9134aa39a2016cb8796e19e6d
kartik raman <kartikraman@gmail.com>
2024-03-15 19:31:03 -0500

    updated name

---
+++ testProject/src/testProject/TopTwofromArray.java
@@ -0,0 +1,16 @@
+package testProject;
+
+import java.util.Arrays;
+
+public class TopTwofromArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] nums = {1,2,6,7,9};
+		System.out.println(Arrays.toString(nums));
+		
+
+	}
+
+}
